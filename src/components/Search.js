26c73d0979d5a887c69275656f57241bbc795bc6
Samuel Cole <sam@samuelcole.name>
2016-08-09 21:40:28 -0400

    copy change
    fixes #20

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,7 +22,7 @@ export default class Search extends React.Component {
 
   render() {
     return <div>
-      <input className="form-control" placeholder="Enter Zip Code" onChange={this.onChange.bind(this)} />
+      <input className="form-control" placeholder="Enter a Keyword, City, or State" onChange={this.onChange.bind(this)} />
     </div>;
   }
 }
