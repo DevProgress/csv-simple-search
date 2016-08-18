23657a93d4a7dd45306117acd3ac2d7458a38f8b
Kahne Raja <kahneraja@gmail.com>
2016-08-18 12:32:26 +1000

    Selenium lint (#63)
    * moved var to const
    
    * fix mocha lint.
    
    * added new test data csv file.

--- test/specs/when-loading.spec.js
+++ test/specs/when-loading.spec.js
@@ -1,38 +1,37 @@
-var assert = require('assert');
+/* eslint-disable prefer-arrow-callback */
+const assert = require('assert');
 
-describe('When loading', function() {
+describe('When loading', function describe() {
+  it('should have the right title', function it() {
+    browser.url('/');
+    const title = browser.getTitle();
+    assert(title === 'CSV Search');
+  });
 
-    it('should have the right title', function () {
-        browser.url('/');
-        var title = browser.getTitle();
-        assert(title === 'CSV Search');
-    });
+  it('should load local csv file', () => function it() {
+    browser.url('/?file=selenium-test-data.csv');
+    browser.waitUntil(function wait() {
+      return !!browser.element('table').value;
+    }, 10000);
+    const rows = browser.elements('table tbody tr').value;
+    assert(rows.length > 1);
+  });
 
-    it('should load local csv file', function () {
-        browser.url('/?file=abc.csv');
-        var $table = browser.waitUntil(function(){
-            return !!browser.element('table').value;
-        }, 10000);
-        const rows = browser.elements('table tbody tr').value;        
-        assert(rows.length > 1);
-    });
+  it('should load external csv file', function it() {
+    browser.url('/?externalFileUrl=http://devprogress.us/csv-simple-search/csv/selenium-test-data.csv');
+    browser.waitUntil(function wait() {
+      return !!browser.element('table').value;
+    }, 10000);
+    const rows = browser.elements('table tbody tr').value;
+    assert(rows.length > 1);
+  });
 
-    it('should load external csv file', function () {
-        browser.url('/?externalFileUrl=http://devprogress.us/csv-simple-search/csv/abc.csv');
-        browser.waitUntil(function(){
-            return !!browser.element('table').value;
-        }, 10000);
-        const rows = browser.elements('table tbody tr').value;        
-        assert(rows.length > 1);
-    });
-
-    it('should show alert with invalid local file', function () {
-        browser.url('/?file=xxx.csv');
-        browser.waitUntil(function(){
-            return !!browser.element('.alert').value;
-        }, 10000);
-        const alert = browser.elements('.alert').value;        
-        assert(!!alert);
-    });    
-
+  it('should show alert with invalid local file', function it() {
+    browser.url('/?file=xxx.csv');
+    browser.waitUntil(function wait() {
+      return !!browser.element('.alert').value;
+    }, 10000);
+    const alert = browser.elements('.alert').value;
+    assert(!!alert);
+  });
 });
--- test/specs/when-searching.js
+++ test/specs/when-searching.js
@@ -1,19 +1,18 @@
-var assert = require('assert');
+/* eslint-disable prefer-arrow-callback */
+const assert = require('assert');
 
-describe('When searching', function() {
+describe('When searching', function describe() {
+  it('"New York" should return one result.', function it() {
+    browser.url('/?file=selenium-test-data.csv');
+    browser.setValue('input', 'New York');
+    const rows = browser.elements('table tbody tr').value;
+    assert(rows.length === 1);
+  });
 
-    it('"New York" should return one result.', function () {
-        browser.url('/');
-        browser.setValue('input', 'New York');
-        const rows = browser.elements('table tbody tr').value;
-        assert(rows.length === 1);
-    });    
-
-    it('"nv flow" should return two results.', function () {
-        browser.url('/');
-        browser.setValue('input', 'nv flow');
-        const rows = browser.elements('table tbody tr').value;
-        assert(rows.length === 2);
-    });
-
+  it('"nv flow" should return two results.', function it() {
+    browser.url('/?file=selenium-test-data.csv');
+    browser.setValue('input', 'nv flow');
+    const rows = browser.elements('table tbody tr').value;
+    assert(rows.length === 2);
+  });
 });
