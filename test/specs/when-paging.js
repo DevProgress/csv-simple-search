ee0633996a0d982814b6bb766b0542deff7ac9e7
kahneraja@gmail.com <kahneraja@gmail.com>
2016-08-18 12:18:03 +1000

    fix mocha lint.

--- test/specs/when-paging.js
+++ test/specs/when-paging.js
@@ -1,11 +1,11 @@
+/* eslint-disable prefer-arrow-callback */
 const assert = require('assert');
 
-describe('When searching', function() {
-
-    it('Should go to next page.', function () {
-        browser.url('/');
-        browser.click('.pager li:nth-child(2)');
-        const rows = browser.elements('table tbody tr').value;
-    });    
-
+describe('When paging', function describe() {
+  it('Should go to next page.', function it() {
+    browser.url('/');
+    browser.click('.pager li:nth-child(2)');
+    const rows = browser.elements('table tbody tr').value;
+    assert(rows.length === 9);
+  });
 });
--- test/specs/when-searching.js
+++ test/specs/when-searching.js
@@ -1,19 +1,18 @@
+/* eslint-disable prefer-arrow-callback */
 const assert = require('assert');
 
-describe('When searching', function() {
+describe('When searching', function describe() {
+  it('"New York" should return one result.', function it() {
+    browser.url('/');
+    browser.setValue('input', 'New York');
+    const rows = browser.elements('table tbody tr').value;
+    assert(rows.length === 1);
+  });
 
-    it('"New York" should return one result.', function () {
-        browser.url('/');
-        browser.setValue('input', 'New York');
-        const rows = browser.elements('table tbody tr').value;
-        assert(rows.length === 1);
-    });    
-
-    it('"nv flow" should return two results.', function () {
-        browser.url('/');
-        browser.setValue('input', 'nv flow');
-        const rows = browser.elements('table tbody tr').value;
-        assert(rows.length === 2);
-    });
-
+  it('"nv flow" should return two results.', function it() {
+    browser.url('/');
+    browser.setValue('input', 'nv flow');
+    const rows = browser.elements('table tbody tr').value;
+    assert(rows.length === 2);
+  });
 });
